c6cea5744c53e41050b255ad1ff96c9627842f20
Jack Weinbender <jack@weinbender.io>
2026-06-29 22:04:17 -0500

    fix(cloudflare): hardcode access policy email in IAC

--- terraform/cloudflare/variables.tf
+++ terraform/cloudflare/variables.tf
@@ -8,8 +8,4 @@ variable "cloudflare_account_id" {
   type        = string
 }
 
-variable "google_oauth_email_whitelist" {
-  description = "List of Google email addresses allowed through Cloudflare Access"
-  type        = list(string)
-}
 
