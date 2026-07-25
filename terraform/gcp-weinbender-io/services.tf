8244cdc1cb05290f801cb67cf6f05462ce58231f
Jack Weinbender <jack@weinbender.io>
2026-07-25 16:59:27 -0500

    Enable Google Workspace APIs (Gmail, Calendar, Drive, Sheets, Docs, People)

--- terraform/gcp-weinbender-io/services.tf
+++ terraform/gcp-weinbender-io/services.tf
@@ -3,7 +3,14 @@ locals {
     "artifactregistry.googleapis.com",
     "cloudresourcemanager.googleapis.com",
     "iam.googleapis.com",
-    "iamcredentials.googleapis.com"
+    "iamcredentials.googleapis.com",
+    # Google Workspace access for Hermes agent (Gmail, Calendar, Drive, Sheets, Docs, Contacts)
+    "gmail.googleapis.com",
+    "calendar-json.googleapis.com",
+    "drive.googleapis.com",
+    "sheets.googleapis.com",
+    "docs.googleapis.com",
+    "people.googleapis.com"
   ]
 }
 
