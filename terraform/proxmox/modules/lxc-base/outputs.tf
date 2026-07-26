0e92415980558e4dd8cd35988876ec0dbcb2e18a
Jack Weinbender <jack.weinbender@gmail.com>
2026-07-25 21:06:56 -0500

    Revert "feat(proxmox): add lxc-base module (hardened, reusable LXC container)"

--- terraform/proxmox/modules/lxc-base/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vm_id" {
-  description = "The Proxmox VMID of the container."
-  value       = proxmox_virtual_environment_container.this.vm_id
-}
-
-output "ipv4_addresses" {
-  description = "Map of IPv4 addresses per network device, as reported by Proxmox (populated once the container has booted and acquired a DHCP lease)."
-  value       = proxmox_virtual_environment_container.this.ipv4
-}
